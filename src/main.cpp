9294eb1cd06a2297207b3575db2321f712b53357
Nikolas Daynard <Nkdaynard@gmail.com>
2024-06-21 18:52:39 -0400

    player movement

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,44 @@
 #include "player.cpp"
 
 int main() {
+    srand(time(NULL));
     initscr();            // Start curses mode
+    curs_set(0); // hide cursor
+    start_color();
+    cbreak();// Disable line buffering
+    noecho(); // Prevent user input from being echoed to the screen
+    keypad(stdscr, TRUE); // Capture special keys like arrow keys 
     char input;
     std::vector<Entity *> entities;
-    entities.push_back(new Player(10, Tile(1, 1, '0')));
+    Player *player = new Player(10, Tile(1, 1, '0'));
+    entities.push_back(player);
     while (input != 'q') {
+        clear();
         for(Entity *entity : entities) {
             entity->render();
         }
         refresh();            // Print it on the real screen
         input = getch();              // Wait for user input
+        switch (input) {
+            case KEY_UP:
+            case 'w':
+                player->tile.pos.y -= 1;
+                break;
+            case KEY_DOWN:
+            case 's':
+                player->tile.pos.y += 1;
+                break;
+            case KEY_LEFT:
+            case 'a':
+                player->tile.pos.x -= 1;
+                break;
+            case KEY_RIGHT:
+            case 'd':
+                player->tile.pos.x += 1;
+                break;
+            default:
+                break;
+        }
     }
     endwin();             // End curses mode
     return 0;
